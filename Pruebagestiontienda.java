45df514b1627711a5aa7fa570fb8c72b7370e7b8
alexisbolivar5 <abf0026@alu.medac.es>
2024-10-24 17:04:47 +0200

    Add files via upload

---
+++ Pruebagestiontienda.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagestiontienda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Pruebagestiontienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String producto = "";
+     
+        
+                double[] precio = new double[11];
+        precio[0] = 1;
+        precio[1] = 2;
+        precio[2] = 3;
+        precio[3] = 3;
+        precio[4] = 4;
+        precio[5] = 5;
+        precio[6] = 6;
+        precio[7] = 7;
+        precio[8] = 8;
+        precio[9] = 9;
+        precio[10] = 10;
+        
+             double[] stock = new double[11];
+        stock[0] = 1;
+        stock[1] = 2;
+        stock[2] = 3;
+        stock[3] = 3;
+        stock[4] = 4;
+        stock[5] = 5;
+        stock[6] = 6;
+        stock[7] = 7;
+        stock[8] = 8;
+        stock[9] = 9;
+        stock[10] = 10;
+        
+        
+
+    }
+}
